d07d5246dce77dc5f5298de349376be53952c24c
AlexGaiser <alexcgaiser@gmail.com>
2021-05-26 22:10:32 -0400

    successfully added typescript support

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
-const app = () => {
-  return <h1>This is my React app! I updated live again</h1>;
+const app = (): JSX.Element => {
+  return (
+    <h1>This is my React app! I updated live again and again</h1>
+  );
 };
 
 const App = hot(module)(app);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   mode: 'development',
   module: {
     rules: [
@@ -11,6 +11,11 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: { presets: ['@babel/env'] },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['ts-loader'],
       },
       {
         test: /\.css$/,
